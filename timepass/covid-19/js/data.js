2233f9601af6d92bfc89b8d6a721ccf1669e2cb3
ipiklu <kundupiklu@gmail.com>
2023-04-30 23:47:47 +0530

    js_v3.6
    v3.6

--- timepass/covid-19/js/data.js
+++ timepass/covid-19/js/data.js
@@ -431,3 +431,5 @@ function drawVisualization() {
           alertify.alert("<a href='https://www.linkedin.com/in/sayantan-kundu-52650758'><img src='img/linkedin.png' style='max-width: 70%; max-height: 70%'/></a><br/><a href='https://twitter.com/piklu21'><img src='img/twitter.png' style='max-width: 30%; max-height: 30%'/></a><a href='https://www.facebook.com/kundupiklu'><img src='img/facebook.jpg' style='max-width: 30%; max-height: 30%'/> </a><a href='https://www.instagram.com/i_piklu/'><img src='img/instagram.png' style='max-width: 30%; max-height: 30%'/></a><p style='color:#EEE; font-style:italic; font-weight:bolder; font-size:25px' />STAY HOME, STAY SAFE.</p><p style='color:#EEE; font-style:italic; font-weight:bolder; font-size:25px' />#Covid-19 #HomeQuarantine</p>");
           return false;
     } 
+	
+	AOS.init();
