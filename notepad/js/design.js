b7678d24a1583e34e1c83187fd1a3b46d81e953b
ipiklu <kundupiklu@gmail.com>
2023-05-08 21:58:19 +0530

    update
    v4.0

---
+++ notepad/js/design.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+
+ <!---POPUP Coustomization---> 
+	  function view() {
+          alertify.alert("<a href='https://www.linkedin.com/in/sayantan-kundu-52650758'><img src='img/linkedin.png' style='max-width: 70%; max-height: 70%'/></a><br/><a href='https://twitter.com/piklu21'><img src='img/twitter.png' style='max-width: 30%; max-height: 30%'/></a><a href='https://www.facebook.com/kundupiklu'><img src='img/facebook.jpg' style='max-width: 30%; max-height: 30%'/> </a><a href='https://www.instagram.com/i_piklu/'><img src='img/instagram.png' style='max-width: 30%; max-height: 30%'/></a><p style='color:#AA205C; font-style:italic; font-weight:bolder; font-size:25px' />STAY HOME, STAY SAFE.</p><p style='color:#AA205C; font-style:italic; font-weight:bolder; font-size:25px' />#Covid-19 #HomeQuarantine</p>");
+          return false;
+    } 
