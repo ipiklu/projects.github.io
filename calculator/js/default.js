aa35d9dbfdc48ea0ca5e230c090be3b3b2b2d52a
ipiklu <kundupiklu@gmail.com>
2023-05-16 23:20:15 +0530

    DesignUpdate
    V5.8

--- calculator/js/default.js
+++ calculator/js/default.js
@@ -22,6 +22,7 @@ function percentage_2() {
 			.value = (num1 * 100) / num2 + "%";
 }
 
+
  <!---POPUP Coustomization---> 
 	  function view() {
           alertify.alert("<a href='https://www.linkedin.com/in/sayantan-kundu-52650758'><img src='img/linkedin.png' style='max-width: 90%; max-height: 70%'/></a><br/><a href='https://twitter.com/piklu21'><img src='img/twitter.png' style='max-width: 30%; max-height: 30%'/></a><a href='https://www.facebook.com/kundupiklu'><img src='img/facebook.jpg' style='max-width: 30%; max-height: 30%'/> </a><a href='https://www.instagram.com/i_piklu/'><img src='img/instagram.png' style='max-width: 30%; max-height: 30%'/></a><p style='color:#AA205C; font-style:italic; font-weight:bolder; font-size:25px' />STAY HOME, STAY SAFE.</p><p style='color:#AA205C; font-style:italic; font-weight:bolder; font-size:25px' />#Covid-19 #HomeQuarantine</p>");
