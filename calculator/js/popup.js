920b8470f4ef402537d94bd7999a912fb63c3adc
Sayantan Kundu <kundupiklu@gmail.com>
2024-01-07 09:55:51 +0530

    calculatorV2.0
    functionality added

---
+++ calculator/js/popup.js
@@ -0,0 +1,4 @@
+// JavaScript Document for simple calculator
+   function fullwindowpopup(){
+      window.open("simple-calculator/index.html","bfs","fullscreen,scrollbars,width=500px,height=447px")
+   }
