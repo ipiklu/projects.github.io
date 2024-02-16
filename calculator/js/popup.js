c6dc792b81d45c55a48d36bd5e15e919ae8ccd13
Sayantan Kundu <kundupiklu@gmail.com>
2024-02-16 23:04:36 +0530

    APP_update_v2.3698.32
    calculator design update

--- calculator/js/popup.js
+++ calculator/js/popup.js
@@ -1,4 +1,4 @@
 // JavaScript Document for simple calculator
    function fullwindowpopup(){
-      window.open("simple-calculator/index.html","bfs","fullscreen,scrollbars,width=500px,height=447px")
+      window.open("simple-calculator/index.html","bfs","fullscreen,scrollbars,width=500px,height=617px")
    }
