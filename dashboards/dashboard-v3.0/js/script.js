02ebe9946eeacc3d1545488a75420881250b8eae
Sayantan Kundu <kundupiklu@gmail.com>
2026-04-11 23:58:36 +0530

    dasboard-v3.0
    v2.0

--- dashboards/dashboard-v3.0/js/script.js
+++ dashboards/dashboard-v3.0/js/script.js
@@ -294,3 +294,28 @@ function reloadClear() {
 }
 
 loadPage('Overview');
+
+<!---POPUP Coustomization---> 
+	  function view() {
+          alertify.alert(`
+            <div style="display:flex; justify-content:center; margin-bottom: 10px;">
+                <a href="https://www.linkedin.com/in/sayantan-kundu-52650758" target="_blank">
+                    <img src="img/linkedin.png" style="max-width: 100%; max-height: 100%;" alt="LinkedIn"/>
+                </a>
+            </div>
+            <div style="display:flex; justify-content:center; align-items:center; gap: 20px; margin-top: 10px;">
+                <a href="https://twitter.com/piklu21" target="_blank">
+                    <img src="img/twitter.png" style="max-width: 150px; width: 100%; height: auto;" alt="Twitter"/>
+                </a>
+                <a href="https://www.facebook.com/kundupiklu" target="_blank">
+                    <img src="img/facebook.jpg" style="max-width: 150px; width: 100%; height: auto;" alt="Facebook"/> 
+                </a>
+                <a href="https://www.instagram.com/i_piklu/" target="_blank">
+                    <img src="img/instagram.png" style="max-width: 150px; width: 100%; height: auto;" alt="Instagram"/>
+                </a>
+            </div>
+            <p class="index-alert" style="font-style:italic; font-weight:bolder; font-size:25px;">STAY HOME, STAY SAFE.</p>
+            <p class="index-alert" style="font-style:italic; font-weight:bolder; font-size:25px;">#Covid-19 #HomeQuarantine</p>
+        `);
+          return false;
+    }
