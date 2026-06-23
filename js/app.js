7c4aec8fcd4ae9c33b4e46f15ca8d2b56d59e827
Sayantan Kundu <kundupiklu@gmail.com>
2026-06-23 23:54:06 +0530

    browserApp
    v1.3

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('./sw.js', { scope: './' })
-      .then(reg => console.log('PWA Ready! Scope is: ', reg.scope))
+      .then(reg => console.log('PWA Ready! Scope is:', reg.scope))
       .catch(err => console.log('PWA Failed', err));
   });
 }
